d2f1580723629fe7dcd3d015b984665f2d9bdb71
DeLaCruzSergioRene <23308060610508@cetis61.edu.mx>
2025-09-24 10:03:30 -0500

    otra agregada

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def hola_mundo():
     return '<h1> ¡Hola mundo desde Flask!</h1>'
 
+@app.route('/otra')
+def hola_mundo1():
+    return '<h1> ¡Hola mundo desde otra ruta!</h1>'
 if __name__ == '__main__':
     app.run(debug=True)
     
