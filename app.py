3954bd8c1a4d573941815fdfc7acf65860570a2b
DeLaCruzSergioRene <23308060610508@cetis61.edu.mx>
2025-09-29 08:26:17 -0500

    cambios 2

--- app.py
+++ app.py
@@ -4,7 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hola_mundo():
-    return '<h1> ¡Hola mundo desde Flask!</h1>'
+    return '''
+<h1>¡Hola mundo desde Flask!</h1>
+<p> Texto random 1 </p>
+<p> Texto random 2 </p>
+<p> Texto random 3 </p>
+<p> Texto random 4 </p>
+<p> Texto random 5 </p>
+'''
+@app.route('/factorial/<v1>')
+def factorial(v1):
+    fact=1
+    for x in range(1, int(v1)):
+        fact*=x
+    return (f"El factorial de {v1}! es {fact}")
 
 @app.route('/otra')
 def hola_mundo1():
